dad9a31768277ec57b87c060b764d8286ca1a273
jessesuihkone <jesse.suihkonen.22@edu.luksia.fi>
2023-03-09 13:41:58 +0200

    raah

--- javascript/Javascript.js
+++ javascript/Javascript.js
@@ -9,9 +9,18 @@
     
 
 
-    alert(course + " and also " + monki + " age over " + age + " and under " + var1 + " at the exception of " + var2);
+   // alert(course + " and also " + monki + " age over " + age + " and under " + var1 + " at the exception of " + var2);
 
     console.log("LOG");
     console.warn("WARNING");
     console.error("ERROR");
 
+
+
+
+
+   
+    var test = document.getElementById("test");
+    var oldtext = test.innerHTML;
+    test.innerHTML = "monkey";
+    var monkey = "bdabadbadbababdbadbaadbadbadbad"
