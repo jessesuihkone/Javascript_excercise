cd72b3d03bb7b86081fe097b5c0a3ea70fab4f8b
jessesuihkone <jesse.suihkonen.22@edu.luksia.fi>
2023-03-09 12:21:33 +0200

    b

---
+++ javascript/Javascript.js
@@ -0,0 +1,17 @@
+    
+    
+    var monki = "monkeys";
+    var course = "Javascript For Beginners";
+    var age = "17";
+    var var1 = "30";
+    var var2 = "30.5";
+    
+
+
+    
+
+
+    var date = new Date();
+
+
+    alert(course + " and also " + monki + " age over " + age + " and under " + var1 + " at the exception of " + var2);
