392ba9cdfb6b8b3203222159d514c2c89ad458c0
jessesuihkone <jesse.suihkonen.22@edu.luksia.fi>
2023-03-10 10:56:29 +0200

    oogabooga

---
+++ kolmassivu/script.js
@@ -0,0 +1,6 @@
+var juujaa = "Donkey Kong";
+var jaajuu = "Konkey Dong";
+var monkey = document.getElementById("monkey");
+var sentence = jaajuu +" Monkey Long " + juujaa;
+
+monkey.innerHTML = "NEWTEXT";
---
+++ tokasivu/script.js
@@ -0,0 +1,17 @@
+//var test = document.getElementById("test");
+//var date = new Date();
+//test.onclick = function (alert) { date; };
+//date.onclick = alert() {new Date()};
+
+var okok = new Date();
+document.getElementById("date").onclick = function() {myFunction()};
+function myFunction() {alert(okok)};
+
+
+//if (confirm('eat banana?')) {
+//   
+//    alert ('monki');
+//  } else {
+//    alert ('not monki');
+//  }
+  
