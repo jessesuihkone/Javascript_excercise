c8a99b73c586f1c71f919d416b80974df5e310d1
jessesuihkone <jesse.suihkonen.22@edu.luksia.fi>
2023-03-23 13:22:41 +0200

    ju

---
+++ Variable_types/script.js
@@ -0,0 +1,11 @@
+var age = 30;
+console.log("Age is:", typeof(age));
+
+var name = "JavaScript 4 Beginners";
+console.log("Name is:", typeof(name));
+
+var isCat = true;
+console.log("isCat is:", typeof(isCat));
+
+var cars = Array("VW", "Lambo", "Audi");
+console.log("Cars is:", typeof(cars));
