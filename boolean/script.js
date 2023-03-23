6e75aeee0b42261d46a8b48c7fe97d1032240402
jessesuihkone <jesse.suihkonen.22@edu.luksia.fi>
2023-03-23 10:27:59 +0200

    Big D powe

---
+++ boolean/script.js
@@ -0,0 +1,36 @@
+var isCat = true;
+
+if(isCat) {
+    console.log("ZEPHYR IS A CAT")
+}
+
+var name = prompt("Enter name")
+
+if(name.toLowerCase() == "Zephyr") {
+    var isCat = true;
+} else {
+    var isCat = false;
+}
+
+
+
+if(isCat) {
+    alert("You're a cat!")
+} else {
+    alert("You're not a cat!")
+}
+
+
+
+if(isCat == false) {
+    alert("not a cat")
+}
+
+
+if(isCat != true) {
+
+}
+
+var isCat = 1;
+var isCat = 0;
+
---
+++ kuudessivu/script.js
@@ -0,0 +1,32 @@
+var familyMember = "Wade";
+
+
+
+
+
+var name = "Doggo";
+
+if(name == "Kalob") {
+    console.log("AWESOME");
+} else if(name == "Zephyr") {
+    console.log("MEOW");
+} else if(name == "Henry") {
+    console.log("Little meow");
+} else {
+    console.warn("Not handled")
+}
+
+
+num1 = 9
+num2 = 1101
+
+console.log(num2+num2);
+console.log(num1 - num2);
+console.log(num1 / num2);
+console.log(num2 * num2);
+console.log(num1 ** num1);
+
+
+console.log(c);
+console.log((c*8)+21);
+console.log(c+32*4);
