bed56a5ade94c9fd3e366c956cc2ae15ca31ab15
jessesuihkone <jesse.suihkonen.22@edu.luksia.fi>
2023-04-19 10:49:58 +0300

    Hoisting

--- Hoisting/script.js
+++ Hoisting/script.js
@@ -1,7 +1,20 @@
 (function getPerson() {
     
-    console.log("Var not set yet", name)
-    var name = "Kalob"
-    console.log("Var is set", name)
+    if(myName === undefined) {
+        myName = "Kalob"
+    }
+
+    console.log("Var not set yet", myName)
+    var myName = "Kalob"
+    console.log("Var is set", myName)
 })();
 
+console.log("Name is", myName)
+
+function getMonk(monkName) {
+    if(monkName === undefined) {
+        monkName = "Monk"
+    }
+    console.log("A thing", monkName)
+    var monkName = "Whatever"
+}
