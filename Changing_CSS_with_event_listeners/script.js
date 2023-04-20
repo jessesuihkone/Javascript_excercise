9a68a19041dbb27ba894176fae9a2a2027960752
jessesuihkone <jesse.suihkonen.22@edu.luksia.fi>
2023-04-20 12:19:33 +0300

    uugabuuga

--- Changing_CSS_with_event_listeners/script.js
+++ Changing_CSS_with_event_listeners/script.js
@@ -1,12 +1,13 @@
 var bgColor = document.querySelector(".js-bg-color")
 var textColor = document.querySelector(".js-text-color")
+var body = document.querySelector("body")
 
 bgColor.addEventListener('keyup', function(event) {
     console.log("bg color", event.target.value)
-    var body = document.querySelector("body")
     body.style.backgroundColor = event.target.value
 });
 
 textColor.addEventListener('keyup', function(event) {
     console.log("text color", event.target.value)
+    body.style.color = event.target.value
 });
