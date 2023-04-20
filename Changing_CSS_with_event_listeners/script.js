738af378814eba67d062e34ab6f5e01a781d37f1
jessesuihkone <jesse.suihkonen.22@edu.luksia.fi>
2023-04-20 12:45:01 +0300

    ju

--- Changing_CSS_with_event_listeners/script.js
+++ Changing_CSS_with_event_listeners/script.js
@@ -1,13 +1,50 @@
+//variables
 var bgColor = document.querySelector(".js-bg-color")
 var textColor = document.querySelector(".js-text-color")
 var body = document.querySelector("body")
 
+//default colors and borders
+body.style.backgroundColor = "navajowhite"
+body.style.borderRadius = "30px"
+
+bgColor.style.borderRadius = "20px"
+bgColor.style.color = "white"
+bgColor.style.backgroundColor = "black"
+
+/*
+body.style.color = "mediumspringgreen"
+body.style.backgroundColor = "black"
+
+bgColor.style.backgroundColor = "black"
+bgColor.style.color = "white"
+
+textColor.style.backgroundColor = "black"
+textColor.style.color = "white"
+*/
+
+// console log input to background color box
 bgColor.addEventListener('keyup', function(event) {
     console.log("bg color", event.target.value)
     body.style.backgroundColor = event.target.value
 });
-
+// console log input to text color box
 textColor.addEventListener('keyup', function(event) {
     console.log("text color", event.target.value)
     body.style.color = event.target.value
 });
+
+// specify what colors change background color
+bgColor.addEventListener('keyup', function(event) {
+    var color = event.target.value
+    switch(color) {
+        case 'red':
+        case 'white':
+        case 'blue':
+            body.style.backgroundColor = color
+            break
+        default:
+            body.style.backgroundColor = "mediumspringgreen"
+            break
+    }
+});
+
