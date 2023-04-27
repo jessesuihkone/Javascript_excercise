e32e7deceddb0fe6af286b8d7f7305094495d422
jessesuihkone <jesse.suihkonen.22@edu.luksia.fi>
2023-04-27 08:49:27 +0300

    qes gem

---
+++ javascript_quessing_game/script.js
@@ -0,0 +1,8 @@
+var nmbr = Number(prompt("Give number"))
+
+while(nmbr != 2222) {
+    console.log("That ain't it")
+    nmbr = Number(prompt("Give number"))
+} if (nmbr == 2222) {
+    console.log("2222 yes very much win:)")
+}
