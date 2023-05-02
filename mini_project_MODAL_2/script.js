90054cd83ac6a72aee88f64dc587cdff25227068
jessesuihkone <jesse.suihkonen.22@edu.luksia.fi>
2023-05-02 13:34:26 +0300

    modal2

---
+++ mini_project_MODAL_2/script.js
@@ -0,0 +1,38 @@
+
+
+class Modal {
+    constructor() {
+        this.modal = document.querySelector(".js-modal")
+    }
+
+    open() {
+        this.modal.style.display = "initial"
+    }
+
+    close() {
+        this.modal.style.display = "none"
+    }
+
+    changeContent(title, text) {
+        if(title !== undefined) {
+            this.modal.querySelector(".js-modal-title").innerText = title
+    }
+        if(text !== undefined) {
+            this.modal.querySelector(".js-modal-text").innerText = text
+        }
+    }
+}
+
+
+const openBtn = document.querySelector(".js-open-modal")
+const closeBtn = document.querySelector(".js-close-modal")
+const modal = new Modal("New title", "New text in here")
+
+
+openBtn.addEventListener("click", function(event) {
+    modal.open()
+})
+
+closeBtn.addEventListener("click", function(event) {
+    modal.close()
+})
