c364d9979410215aa87711b5a7fd47b4eff595f6
jessesuihkone <jesse.suihkonen.22@edu.luksia.fi>
2023-05-22 09:22:09 +0300

    efwefwefewfw
    fwefwefewfwe

---
+++ arrow_functions/script.js
@@ -0,0 +1,48 @@
+// (function somename(one, two) {
+//     return athing
+// })();
+// const kalob = function() {
+
+//     return "kalob"
+// }
+
+// function something() {
+//     this
+// }
+// class NewClass {
+//     funcname() {
+//         this
+//     }
+// }
+// const person = {
+//     funcname: function() {
+//         this
+//     }
+// }
+
+// greet = (name, place) => {
+//     return `Hello ${name}. Welcome to ${place}`
+// }
+// const hi = greet("Zephyr", "Atlantis")
+// console.log(hi)
+
+// greet = name => `Hello ${name}, ${place}`
+
+// const hi = greet("Henry", "Canada")
+// console.log(hi)
+
+const arr = ["JavaScript", "Python", "PHP"]
+
+arr.forEach(language => {
+    console.log(language)
+});
+
+setTimeout(() => {
+    console.log("YAY I WAITED")
+    console.log("YAY I WAITED")
+    console.log("YAY I WAITED")
+    console.log("YAY I WAITED")
+    console.log("YAY I WAITED")
+    console.log("YAY I WAITED")
+    console.log("YAY I WAITED")
+}, 1000);
