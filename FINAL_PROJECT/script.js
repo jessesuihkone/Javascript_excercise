7e64dd1357fd930c9973bc0e7e90a1b8887dbdcb
jessesuihkone <jesse.suihkonen.22@edu.luksia.fi>
2023-05-25 13:34:38 +0300

    lol

---
+++ FINAL_PROJECT/script.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var header1 = document.getElementById("header1").innerText;
+
+
+var charac = header1.split("");
+
+var f = charac[0];
+var i = charac[1];
+var n = charac[2];
+var a = charac[3];
+var l = charac[4];
+var space = charac[5];
+var p = charac[6];
+var r = charac[7];
+var o = charac[8];
+var j = charac[9];
+var e = charac[10];
+var c = charac[11];
+var t = charac[12];
+
+ftitle = [f + i + n + a + l + space + p + r + o + j + e + c + t];
+
+//const element = document.getElementById("header1");
+
+ftitle.forEach(charac => {
+    console.log("leter")
+    .addEventListener("mouseover", myFunction);  
+});
+
+
+
+function myFunction() {
+    
+  }
+*/
